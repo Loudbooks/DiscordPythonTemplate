b540eba6972df57bd32271e19326874785eb85fc
Loudbook <loudbooked@gmail.com>
2021-06-17 10:35:11 -0400

    Update jayden.py

--- cogs/jayden.py
+++ cogs/jayden.py
@@ -8,20 +8,7 @@ class Jayden(commands.Cog):
         self.client = client
     @commands.Cog.listener()
     async def on_ready(self):
-        print('Jayden machine is loaded.')
+        print('COG is loaded!')
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.id == 854156802325479434:
-            return
-        elif 'ur mom' in message.content:
-            await message.channel.send('Ur mom actually came back from buying cigarettes.')
-        elif 'Wiccan' in message.content:
-            await message.channel.send('Wiccan is inferior to I')
-        elif 'funsies' in message.content:
-            await message.channel.send('Are you just trying to ruin Reubens life?')
-        elif 'dead chat' in message.content:
-            await message.channel.send('Only dead because we are hiding from you.')
-   
 def setup(client):
     client.add_cog(Jayden(client))
